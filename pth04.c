0c386e9a9c2d87452e4e0d18dc9b9ef19ab505a9
zyb <zyb0408@gmail.com>
2019-01-01 19:32:37 +0800

    修改注释

--- pth04.c
+++ pth04.c
@@ -2,12 +2,12 @@
 #include<stdlib.h>
 #include<pthread.h>
 
-void *pth_inc(void *);							//增加1函数
-void *pth_dec(void *);							//减少1函数
-int num = 500, loop = 5;						//num为指定数，loop为循环次数，即+1，-1的次数
+void *pth_inc(void *);											//增加1函数
+void *pth_dec(void *);											//减少1函数
+int num = 500, loop = 5;										//num为指定数，loop为循环次数，即+1，-1的次数
 pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
 int main(int argc, char *argv[]) {
-	pthread_t tid1, tid2;						//线程id
+	pthread_t tid1, tid2;										//线程id
 	printf("pthread mutex_lock demonstration\n");
 	printf("A number=%d\n",num);
 //1 创建逐个+1的线程
@@ -34,7 +34,7 @@ void * pth_inc(void *arg){
 	for (i = 0; i < loop; i++){
 		j = num; sleep(1); num++;
 	}
-	pthread_mutex_unlock(&mtx);								//释放互斥锁
+	pthread_mutex_unlock(&mtx);									//释放互斥锁
 }
 //6 逐-1函数
 void * pth_dec(void *arg) {
@@ -44,5 +44,5 @@ void * pth_dec(void *arg) {
 	for (i = 0; i < loop; i++) {
 		j = num; sleep(1); num=j-1;
 	}
-	pthread_mutex_unlock(&mtx);								//释放互斥锁
+	pthread_mutex_unlock(&mtx);									//释放互斥锁
 }
